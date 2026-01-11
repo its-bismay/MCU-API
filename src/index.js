5394eefff53d8487b33e873d542a326d895805fc
Bibhabasu Bismay Kumar Naik <155875271+its-bismay@users.noreply.github.com>
2026-01-11 23:23:14 +0530

    Add JSON parsing middleware to Express app

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import express from "express"
 
 const app = express();
 
+app.use(express.json())
+
 app.get("/", (_req, res) => {
     res.send("Server is up and running")
 });
+
 
 
 export default app;
